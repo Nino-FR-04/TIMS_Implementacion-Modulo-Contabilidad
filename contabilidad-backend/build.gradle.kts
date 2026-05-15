8dda272a293e7cdbf7ea7763707c3e7dfa3d3037
Nino-FR-04 <davidfrx322@gmail.com>
2026-05-15 04:01:06 -0500

    Correcciones minimas

--- contabilidad-backend/build.gradle.kts
+++ contabilidad-backend/build.gradle.kts
@@ -1,15 +1,35 @@
 plugins {
     id("java")
+    kotlin("jvm") version "1.9.22"
+    kotlin("plugin.spring") version "1.9.22"
+    kotlin("plugin.jpa") version "1.9.22"
+    id("org.springframework.boot") version "3.2.0"
+    id("io.spring.dependency-management") version "1.1.4"
 }
 
 group = "org.contabilidad"
 version = "1.0-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+
+    // Spring Web
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // JPA y MySQL
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("com.mysql:mysql-connector-j")
+
+    // Lombok
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+
+    // Testing
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
