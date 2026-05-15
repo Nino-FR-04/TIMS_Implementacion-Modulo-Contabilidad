9feab8c1eee7c1372eae28b5186d9757f841126d
Nino-FR-04 <davidfrx322@gmail.com>
2026-05-15 02:56:34 -0500

    Creacion de la carpeta config: implementacion de la configuración CORS para permitir la comunicacion entre el frontend y el backend, definiendo orígenes, metodos HTTP y encabezados permitidos

--- contabilidad-backend/src/main/java/org/contabilidad/config/CorsConfig.java
+++ contabilidad-backend/src/main/java/org/contabilidad/config/CorsConfig.java
@@ -1,4 +1,24 @@
 package org.contabilidad.config;
 
+import org.jetbrains.annotations.NotNull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
 public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NotNull CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET","POST","PUT","DELETe","OPTIONS")
+                        .exposedHeaders("*");
+            }
+        };
+    }
 }
